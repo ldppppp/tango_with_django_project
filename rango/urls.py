63fc2397430a46e10fe49e80af1155ca3c702886
Rui Sun <2664218S@student.gla.ac.uk>
2022-02-11 12:17:30 +0800

    first commit

---
+++ rango/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('admin/', admin.site.urls),
+]
