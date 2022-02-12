d1333f3c142659e62caa43f45d26787f0ae597d3
Rui Sun <2664218S@student.gla.ac.uk>
2022-02-12 22:45:12 +0800

    Fourth commit

--- rango/models.py
+++ rango/models.py
@@ -1,20 +1,26 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=1)
+    likes = models.IntegerField(default=1)
+    slug = models.SlugField(max_length=256, default='')
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.CharField(max_length=128)
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default=1)
 
     def __str__(self):
         return self.title
