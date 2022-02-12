d427812bf39412b95019e81b00adcb516227fd30
ldppppp <99024859+ldppppp@users.noreply.github.com>
2022-02-12 14:46:27 +0800

    Update models.py

--- rango/models.py
+++ rango/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=1)
+    likes = models.IntegerField(default=1)
 
     def __str__(self):
         return self.name
@@ -14,7 +14,7 @@ class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.CharField(max_length=128)
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default=1)
 
     def __str__(self):
         return self.title
